9b95d0cfcb7b22f363bd52f70fa92c95d186de3e
“[Nardin <194287192+camus-coding@users.noreply.github.com>
2026-03-29 16:01:11 +0200

    Final version of summary.sh copying all output files and stopping container

--- summary.sh
+++ summary.sh
@@ -4,14 +4,10 @@ echo "Creating results folder..."
 mkdir -p results
 
 echo "Copying CSV files..."
-cp data/data_raw.csv results/
-cp data/data_cleaned.csv results/
+cp data/*.csv results/
 
 echo "Copying TXT files..."
-cp data/insight1.txt results/
-cp data/insight2.txt results/
-cp data/insight3.txt results/
-cp data/clusters.txt results/
+cp data/*.txt results/
 
 echo "Copying PNG files..."
 cp data/*.png results/
