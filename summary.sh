d336873b04f810e373ddc81f59e1676144a302a2
“[Nardin <194287192+camus-coding@users.noreply.github.com>
2026-03-29 15:57:30 +0200

    Updated summary.sh to copy all output files

--- summary.sh
+++ summary.sh
@@ -3,11 +3,23 @@
 echo "Creating results folder..."
 mkdir -p results
 
-cp *.csv results/
-cp *.txt results/
-cp *.png results/
+echo "Copying CSV files..."
+cp data/data_raw.csv results/
+cp data/data_cleaned.csv results/
 
+echo "Copying TXT files..."
+cp data/insight1.txt results/
+cp data/insight2.txt results/
+cp data/insight3.txt results/
+cp data/clusters.txt results/
+
+echo "Copying PNG files..."
+cp data/*.png results/
+
+echo "Stopping container..."
 docker stop bigdata-container
+
+echo "Removing container..."
 docker rm bigdata-container
 
 echo "Summary script finished."
